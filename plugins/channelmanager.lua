dedd0cae6cac3873a28d9e9988d465bbddd025a3
Shahab <safaeishahab@yahoo.com>
2016-05-21 13:22:07 +0430

    Update channelmanager.lua

--- plugins/channelmanager.lua
+++ plugins/channelmanager.lua
@@ -1,8 +1,8 @@
   local function run(msg, matches)
-      if is_sudo(msg.from.peer_id) then
+      if is_sudo(msg) then
   local web_preview = '&disable_web_page_preview='..(tostring(disable_web_page_preview) or '')
   local markdown = '&parse_mode='..('markdown' or 'HTML')
-  local url = 'https://api.telegram.org/bot'..token..'/sendMessage'
+  local url = 'https://api.telegram.org/bot'213879334:AAHrNB4EQsLTmJ2SXKN1TEkFJd8r-zffN2g'/sendMessage'
   local response = {}
 local text = matches[1]
   local res, code = https.request{
